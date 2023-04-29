59a3029fd7f12631ed3522e6ba6d33dfc93e840a
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2023-04-29 18:09:08 -0300

    hard code, sorry :(

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
     const db = require('../db');
     console.log('mysql started');
 
-    // INSERT users
     console.log('INSERT INTO users');
     const resultInsert = await db.insertUsers({
         firstname: 'Ronan',
@@ -12,12 +11,10 @@
     });
     console.log(resultInsert);
 
-    // SELECT all users
     console.log('SELECT * FROM users');
     const users = await db.selectUsers();
     console.log(users);
 
-    // UPDATE users
     console.log('UPDATE users');
     const resultUpdate = await db.updateUsers(3, {
         firstname: 'Nilso',
@@ -27,7 +24,6 @@
     });
     console.log(resultUpdate);
 
-    // DELETE users
     console.log('DELETE FROM users');
     const resultDelete = await db.deleteUsers(9);
     console.log(resultDelete);
