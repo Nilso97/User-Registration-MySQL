1aa8ec7e0aedf2e6f880af91a3b8161db3585a2e
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2023-04-29 18:06:33 -0300

    tentando ajeitar a bagunça no código

--- db.js
+++ db.js
@@ -11,24 +11,6 @@ async function connect() {
     return connection;
 }
 
-// connect();
-
-/* 
-DATABASE: DB_userRegistration  
-TABLE: users
-
-   |  ID  |  FIRSTNAME	|    LASTNAME	      |       EMAIL	                     |  PASSWORD  |
-   |  1	  | Claudio 	| Oliveira	          | claudiomsi@hotmail.com	         |   ******   |
-   |  2	  | Fabio	    | José	              | fabiosilvarodolpho@gmail.com     |   ******   |
-   |  3	  | Fernando 	| Eduardo de Souza    | fernando.financeiro@yahoo.com.br |   ******   |
-   |  4	  | João 	    | Moura	              | joaomoura.com@gmail.com	         |   ******   |
-   |  5	  | Luciano 	| Santos	          | luciano.santos@uol.com.br	     |   ******   |
-   |  6	  | Mariana 	| Figueiredo	      | maripitanga@yahoo.com.br	     |   ******   |
-   |  7	  | Roberto 	| Paiva	              | robertopaivaf@hotmail.com	     |   ******   |
-   |  8	  | Ronan 	    | Gonçalves Figuerêdo | ronangoncalves@gmail.com	     |   ******   |
-*/
-
-// INSERT users
 async function insertUsers(user) {
     const conn = await connect();
     const sql = 'INSERT INTO users(firstname, lastname, email, password) VALUES (?,?,?,?);';
@@ -36,14 +18,12 @@ async function insertUsers(user) {
     return await conn.query(sql, values);
 }
 
-// SELECT all users
 async function selectUsers() {
     const conn = await connect();
     const [rows] = await conn.query('SELECT * FROM users;');
     return await rows;
 }
 
-// UPDATE users
 async function updateUsers(id, user) {
     const conn = await connect();
     const sql = 'UPDATE users SET firstname=?, lastname=?, email=?, password=? WHERE id=?;';
@@ -51,7 +31,6 @@ async function updateUsers(id, user) {
     return await conn.query(sql, values);
 }
 
-// DELETE users
 async function deleteUsers(id) {
     const conn = await connect();
     const sql = 'DELETE FROM users WHERE id=?;';
